b6e7af328262493c255228f437768d2648cb8454
Maryna Medynska <maryna.medynska@gmail.com>
2023-01-08 15:16:43 +0200

    Add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector("[data-start]");
+const buttonStop = document.querySelector("[data-stop]");
+const body = document.querySelector('body');
 
+let timerId = null;
+
+buttonStart.addEventListener("click", () => {
+    buttonStart.disabled = true;
+    timerId = setInterval(() => {
+        let randomColor = getRandomHexColor();
+        body.style.backgroundColor = `${randomColor}`;
+        console.log(randomColor);
+    }, 1000);
+});
+
+buttonStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
